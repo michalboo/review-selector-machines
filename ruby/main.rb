8ac88fd258df4a955ffc74d57dd687dd7a7efa7a
Michał Buś <michal.bus@gmail.com>
2018-09-26 15:11:03 +0100

    Get rid of some single-use methods

--- ruby/main.rb
+++ ruby/main.rb
@@ -2,45 +2,29 @@ require "oj"
 require "httparty"
 require "time"
 
-def load_reviewers
- return Oj.load(File.read(@reviewer_path))
+def reset_list(reviewers)
+  reviewers.each { |reviewer| reviewer["reviewing"] = true }
+  return reviewers
 end
 
-def dump_reviewers
-  File.open(@reviewer_path, 'w') { |file| file.write(Oj.dump(@reviewers, indent: 2))}
-end
+reviewer_path = "../reviewers.json"
+reviewers = Oj.load(File.read(reviewer_path))
+config = Oj.load(File.read("../config.json"))
 
-def reset_list
-  @reviewers.each { |reviewer| reviewer["reviewing"] = true }
-  return @reviewers
-end
+selected = 
+  reviewers.shuffle.find { |reviewer| reviewer["reviewing"] == true } ||
+  reset_list(reviewers).shuffle.find { |reviewer| reviewer["reviewing"] == true }
 
-def select_reviewer
-  @available_reviewers = @reviewers.select { |reviewer| reviewer["reviewing"] == true }
-  if @available_reviewers.empty?
-    @available_reviewers = reset_list
-  end
-  selection = @available_reviewers.sample
-  selection["reviewing"] = false
-  selection["last_selection"] = Time.now.utc.iso8601
-  return selection
-end
+selected["reviewing"] = false
+selected["last_selection"] = Time.now.utc.iso8601
+puts selected
 
-def notify_slack(webhook_url, name)
-  result = HTTParty.post(webhook_url,
-    body: Oj.dump({
-      "text" => "Ruby says: #{name}",
-      "channel" => "#echo-chamber"
-    }),
-    headers: { "Content-Type" => "application/json" }
-  )
-end
+HTTParty.post(config["SLACK_WEBHOOK_URL"],
+  body: Oj.dump({
+    "text" => "Ruby says: #{selected["name"]}",
+    "channel" => "#ehco"
+  }),
+  headers: { "Content-Type" => "application/json" }
+)
 
-@reviewer_path = "../reviewers.json"
-@config = Oj.load(File.read("../config.json"))
-
-@reviewers = load_reviewers
-selected = select_reviewer
-
-notify_slack(@config["SLACK_WEBHOOK_URL"], selected["name"])
-dump_reviewers
+File.open(reviewer_path, 'w') { |file| file.write(Oj.dump(reviewers, indent: 2))}
